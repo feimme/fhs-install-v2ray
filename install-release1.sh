9c67829dcbe996957e2d41c61a7114aefeebd1cf
feimme <36754464+feimme@users.noreply.github.com>
2023-01-06 14:25:45 +0800

    Add files via upload

--- install-release1.sh
+++ install-release1.sh
@@ -540,11 +540,7 @@ main() {
     if [[ "$NUMBER" -eq '0' ]] || [[ "$FORCE" -eq '1' ]] || [[ "$NUMBER" -eq 2 ]]; then
       echo "info: Installing V2Ray $RELEASE_VERSION for $(uname -m)"
       download_v2ray
-      if [[ "$?" -eq '1' ]]; then
-        "rm" -r "$TMP_DIRECTORY"
-        echo "removed: $TMP_DIRECTORY"
-        exit 1
-      fi
+      
       install_software 'unzip' 'unzip'
       decompression v2ray-linux-64.zip
     elif [[ "$NUMBER" -eq '1' ]]; then
