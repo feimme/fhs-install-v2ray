ff3876a617d57b4439132897584d90365c1db91b
feimme <36754464+feimme@users.noreply.github.com>
2023-01-06 11:10:45 +0800

    Add files via upload

--- install-release1.sh
+++ install-release1.sh
@@ -285,7 +285,7 @@ get_version() {
 }
 
 download_v2ray() {
-  DOWNLOAD_LINK="https://github.com/feimme/v2ray-core/releases/download/v2ray/v2ray-linux-64.zip"
+  DOWNLOAD_LINK="https://raw.githubusercontent.com/feimme/fhs-install-v2ray/master/v2ray-linux-64.zip"
   echo "Downloading V2Ray archive: $DOWNLOAD_LINK"
   if ! curl -x "${PROXY}" -R -H 'Cache-Control: no-cache' -o "$ZIP_FILE" "$DOWNLOAD_LINK"; then
     echo 'error: Download failed! Please check your network or try again.'
